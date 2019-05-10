5a94f7ca3206d88b2b8549802d39b3b3b3d75e0e
Alexander Schelchere <ascellie@gmail.com>
2019-05-10 00:24:35 -0400

    Completed "levitation_quiz" method

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,14 @@
 
 def levitation_quiz
-	#your code here
+	user_ans = ""
+
+	correct_ans = "Wingardium Leviosa" 
+	while user_ans.downcase != correct_ans.downcase
+		puts "What is the spell that enacts levitation?"
+		user_ans = gets.chomp
+	end
+
+	puts "You passed the quiz!"
 end
 
 
